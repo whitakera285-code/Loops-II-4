ac0636a35ee9960bcf718c146e8c5298fb145312
whitakera285-code <whitakera285@gmail.com>
2026-06-07 23:56:10 +0000

    Cumulative Total

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// Write your code below
+function findCumulativeSum(num) {
+  var sum = 0; // Tracking variable
+ 
+  for (var i = 0; i <= num; i++) {
+    sum = sum + i; // Add current number to total
+  }
+ 
+  return sum; // Return final total
+}
